2e66b2fe5925e3e1c79822156f8ad510c848e980
jfkahn <ioniontech@yahoo.com>
2014-11-14 10:22:36 -0600

    alarm functionality works and removed backgrounder module as it was causing cordova ios build errors.

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,7 @@ angular.module('zeez', ['ionic'])
 
   $scope.toggleLights = function() {
     console.log("Lights toggled, fuck yeah")
-    $http.put("http://localhost:56780/lights/all/toggle")
+    $http.put("http://lifx-http.local:56780/lights/all/toggle")
   };
   // Store Sleep and Wake times
 
@@ -22,8 +22,9 @@ angular.module('zeez', ['ionic'])
     //the timeout function below calls sunrise in milliseconds
 
     var timeoutPromise = $timeout(function(){$scope.simulateSunRise()}, $scope.timeToSunrise($scope.wake));
-
+    $scope.turnLightsOff();
     console.log($scope.wake);
+    // set new pre sleep time when user goes to sleep
    };
 
    $scope.setPreSleep = function () {
@@ -50,22 +51,20 @@ angular.module('zeez', ['ionic'])
       }
    };
 
-   $scope.testPreSleep = function() {
-      console.log("this worked!")
-   };
+
 
    $scope.timeToSunrise = function(wakeTime){
       //get milliseconds between now and set wake time tomorrow
       $scope.currentTime = moment().format("HH:mm:s a");
       $scope.now = moment();
-      $scope.wakeTime = moment(wakeTime, "HH:mm")
+      $scope.wakeTime = moment(wakeTime, "HH:mm");
       // get the difference in ms between waketime and now
-      $scope.diffMs = $scope.wakeTime.diff($scope.now)
+      $scope.wakeDiffMs = $scope.wakeTime.diff($scope.now);
 
       //if diffms is negative, meaning the current time is after the wake time, add one day to wake time
-      if ($scope.diffMs < 0) {
+      if ($scope.wakeDiffMs < 0) {
         $scope.wakeTime.add(1, 'days')
-        $scope.diffMs = $scope.wakeTime.diff($scope.now)
+        $scope.wakeDiffMs = $scope.wakeTime.diff($scope.now)
         return $scope.wakeDiffMs
       }
       else {
@@ -79,26 +78,31 @@ angular.module('zeez', ['ionic'])
 
     $scope.activatePreSleep = function(){
       $scope.preSleepLights();
-      $scope.showSleepButton();
+     // $scope.showSleepButton();
     };
 
     $scope.simulateSunRise = function () {
       console.log("The sun rise is happening");
-      $http.put("http://localhost:56780/lights/all/toggle")
+      $http.put("http://lifx-http.local:56780/lights/all/color?hue=230&saturation=0.19&brightness=1");
+      $http.put("http://lifx-http.local:56780/lights/all/on");
       //lights should come on, intensifying to blue 20 minutes before wake up time
       //10 minutes after wakeup they should change to more white
     };
 
     $scope.preSleepLights = function () {
+      //get all lights ready for pre-sleep
       console.log("Getting you ready for sleep")
-      $http.put("http://localhost:56780/lights/all/toggle")
+      $http.put("http://lifx-http.local:56780/lights/all/color?hue=26.68&saturation=1.0&brightness=.39")
     };
 
     $scope.showSleepButton = function () {
       console.log("Sleep button appeared")
     };
 
-
+    $scope.turnLightsOff = function() {
+      console.log("going to sleep and turning lights off")
+      $http.put("http://lifx-http.local:56780/lights/all/off");
+    }
 
 } );
 
